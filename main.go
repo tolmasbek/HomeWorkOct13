ef4a7970da99e20279cda47aeb9fced68b7c0c3c
tolmasbek <farmonovtt@gmail.com>
2020-10-15 19:41:42 +0400

    did hw and put Println

--- main.go
+++ main.go
@@ -6,4 +6,21 @@ import (
 
 func main() {
 	
+	creditSum := 1000
+	percent := 9
+	limit := 2500
+	month := 0
+	
+	for creditSum <= limit {
+		
+		fmt.Println("В месяце номер ", month)
+		sumPercent := creditSum * percent / 100
+
+		fmt.Println("Сумма процента составляет ", sumPercent)
+		creditSum = creditSum + sumPercent
+
+		month ++
+	}
+	
+	fmt. Println("Общая сумма кредита:",creditSum, "\nМесячная процентная ставка: 9%\nlimit: 2500\nПревысило за: ", month, "месяцев.")
 }
